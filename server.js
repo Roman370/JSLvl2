b0d196151c4805f461d74f17dc0e5e7d8e364b40
Roman370 <romatchernow@yandex.ru>
2020-11-14 18:18:24 +0400

    lesson1; vip

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+
+    let body;
+for(let i = 0; i<=5; i++) {
+    switch(req.url) {
+
+        case"/":
+        body = fs.readFileSync(`./public/index.html`);
+        break;
+        case"/style.css":
+        body = fs.readFileSync(`./public/style.css`);
+        break;
+        case"/main.js":
+        body = fs.readFileSync(`./public/main.js`);
+        break;
+        case"/img/computer-apple3.jpg":
+        body = fs.readFileSync(`./public/img/computer-apple3.jpg`);
+        break;
+        case"/img/computer.jpg":
+        body = fs.readFileSync(`./public/img/computer.jpg`);
+        break;
+        case"/img/fon.jpg":
+        body = fs.readFileSync(`./public/img/fon.jpg`);
+        break;
+        case"/img/Logo.jpg":
+        body = fs.readFileSync(`./public/img/Logo.jpg`);
+        break;
+        
+    }
+
+}
+    
+    res.end(body)
+
+})
+
+const port = process.env.PORT || 3000
+server.listen(port)
+
+console.log('Server started!')
